e93acb4f8af0bd36d7cf5ee89c203e16dd38e74e
Snehal <nanavaresnehal115@gmail.com>
2022-09-24 15:31:49 +0530

    Java Script Programming Construct

---
+++ If-else-else-if/ArithmeticOperation.js
@@ -0,0 +1,16 @@
+
+const a = 10
+const b = 20
+const c = 30
+
+//performing operation
+let result1 = (a + (b * c));
+let result2 = ((a % b) + c);
+let result3 = (c + (a / b));
+let result4 = ((a * b) + c);
+
+//printing the result from it
+console.log(" \n a+b*c = " + result1);
+console.log("a%b+c = " + result2);
+console.log("c+a/b = " + result3);
+console.log("a*b+c = " + result4);
